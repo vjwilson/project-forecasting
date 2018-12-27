caab3aabe5db00331eb6494c8971482d5aed9dd1
Van J. Wilson <van.wilson1992@alumni.unc.edu>
2018-12-27 09:56:04 -0500

    Add alternate table view for A/B testing

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -23,7 +23,212 @@ class Dashboard extends Component {
           <ConsultantGroup />
         )}
         { role === 'Practice Manager' && (
-          <p>I am a Practice Manager and oversee a team of 15 people. As a Practice Manager, it is my responsibility to monitor the practice’s utilization rate and alert the Operations team if we are at risk of not hitting our numbers, or if someone has been on the bench for an extended period of time.</p>
+          <div className={styles['card']}>
+            <table className={styles['consultant-table']}>
+              <thead>
+                <tr>
+                  <th className={styles['left-cell']}>Consultant</th>
+                  <th className={styles['left-cell']}>Level</th>
+                  <th className={styles['left-cell']}>Title</th>
+                  <th className={styles['right-cell']}>This Week</th>
+                  <th className={styles['right-cell']}>Next Week</th>
+                  <th className={styles['right-cell']}>7-Jan</th>
+                  <th className={styles['right-cell']}>14-Jan</th>
+                  <th className={styles['right-cell']}>21-Jan</th>
+                  <th className={styles['right-cell']}>28-Jan</th>
+                  <th className={styles['right-cell']}>4-Feb</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr className={styles['body-row']}>
+                  <td className={styles['left-cell']}>Karissa Martindale</td>
+                  <td className={styles['left-cell']}>Practice Manager</td>
+                  <td className={styles['left-cell']}>Front-end Developer</td>
+                  <td className={styles['right-cell']}>40</td>
+                  <td className={styles['right-cell']}>40</td>
+                  <td className={styles['right-cell']}>40</td>
+                  <td className={styles['right-cell']}>40</td>
+                  <td className={styles['right-cell']}>40</td>
+                  <td className={styles['right-cell']}>40</td>
+                  <td className={styles['right-cell']}>40</td>
+                </tr>
+                <tr className={styles['body-row']}>
+                  <td className={styles['left-cell']}>Lesley Luciano</td>
+                  <td className={styles['left-cell']}>Principal II</td>
+                  <td className={styles['left-cell']}>UX Architect</td>
+                  <td className={styles['right-cell']}>40</td>
+                  <td className={styles['right-cell']}>40</td>
+                  <td className={styles['right-cell']}>40</td>
+                  <td className={styles['right-cell']}>40</td>
+                  <td className={styles['right-cell']}>40</td>
+                  <td className={styles['right-cell']}>40</td>
+                  <td className={styles['right-cell']}>40</td>
+                </tr>
+                <tr className={styles['body-row']}>
+                  <td className={styles['left-cell']}>Matthew Tallman</td>
+                  <td className={styles['left-cell']}>Practice Manager</td>
+                  <td></td>
+                  <td className={styles['right-cell']}>40</td>
+                  <td className={styles['right-cell']}>40</td>
+                  <td className={styles['right-cell']}>40</td>
+                  <td className={styles['right-cell']}>40</td>
+                  <td className={styles['right-cell']}>40</td>
+                  <td className={styles['right-cell']}>40</td>
+                  <td className={styles['right-cell']}>40</td>
+                </tr>
+                <tr className={styles['body-row']}>
+                  <td className={styles['left-cell']}>Sarah Hudson</td>
+                  <td className={styles['left-cell']}>Senior II</td>
+                  <td className={styles['left-cell']}>Front-end Developer</td>
+                  <td className={styles['right-cell']}>40</td>
+                  <td className={styles['right-cell']}>40</td>
+                  <td className={styles['right-cell']}>40</td>
+                  <td className={styles['right-cell']}>40</td>
+                  <td className={styles['right-cell']}>40</td>
+                  <td className={styles['right-cell']}>40</td>
+                  <td className={styles['right-cell']}>40</td>
+                </tr>
+                <tr className={styles['body-row']}>
+                  <td className={styles['left-cell']}>Andria Wiser</td>
+                  <td className={styles['left-cell']}>Senior I</td>
+                  <td className={styles['left-cell']}>Designer</td>
+                  <td className={styles['right-cell']}>40</td>
+                  <td className={styles['right-cell']}>40</td>
+                  <td className={styles['right-cell']}>40</td>
+                  <td className={styles['right-cell']}>40</td>
+                  <td className={styles['right-cell']}>40</td>
+                  <td className={styles['right-cell']}>40</td>
+                  <td className={styles['right-cell']}>40</td>
+                </tr>
+                <tr className={styles['body-row']}>
+                  <td className={styles['left-cell']}>Ariana Sutton</td>
+                  <td className={styles['left-cell']}>Staff II</td>
+                  <td className={styles['left-cell']}>Designer</td>
+                  <td className={styles['right-cell']}>40</td>
+                  <td className={styles['right-cell']}>40</td>
+                  <td className={styles['right-cell']}>40</td>
+                  <td className={styles['right-cell']}>40</td>
+                  <td className={styles['right-cell']}>40</td>
+                  <td className={styles['right-cell']}>40</td>
+                  <td className={styles['right-cell']}>40</td>
+                </tr>
+                <tr className={styles['body-row']}>
+                  <td className={styles['left-cell']}>Ellen Boyette</td>
+                  <td className={styles['left-cell']}>Staff II</td>
+                  <td className={styles['left-cell']}>Front-end Developer</td>
+                  <td className={styles['right-cell']}>40</td>
+                  <td className={styles['right-cell']}>40</td>
+                  <td className={styles['right-cell']}>40</td>
+                  <td className={styles['right-cell']}>40</td>
+                  <td className={styles['right-cell']}>40</td>
+                  <td className={styles['right-cell']}>40</td>
+                  <td className={styles['right-cell']}>40</td>
+                </tr>
+                <tr className={styles['body-row']}>
+                  <td className={styles['left-cell']}>Erica Mitchell</td>
+                  <td className={styles['left-cell']}>Staff I</td>
+                  <td className={styles['left-cell']}>Front-end Developer</td>
+                  <td className={styles['right-cell']}>40</td>
+                  <td className={styles['right-cell']}>40</td>
+                  <td className={styles['right-cell']}>40</td>
+                  <td className={styles['right-cell']}>40</td>
+                  <td className={styles['right-cell']}>40</td>
+                  <td className={styles['right-cell']}>40</td>
+                  <td className={styles['right-cell']}>40</td>
+                </tr>
+                <tr className={styles['body-row']}>
+                  <td className={styles['left-cell']}>Gary Meyer</td>
+                  <td className={styles['left-cell']}>Staff I</td>
+                  <td className={styles['left-cell']}>Designer</td>
+                  <td className={styles['right-cell']}>40</td>
+                  <td className={styles['right-cell']}>40</td>
+                  <td className={styles['right-cell']}>40</td>
+                  <td className={styles['right-cell']}>40</td>
+                  <td className={styles['right-cell']}>40</td>
+                  <td className={styles['right-cell']}>40</td>
+                  <td className={styles['right-cell']}>40</td>
+                </tr>
+                <tr className={styles['body-row']}>
+                  <td className={styles['left-cell']}>Kiley LoCascio</td>
+                  <td className={styles['left-cell']}></td>
+                  <td className={styles['left-cell']}></td>
+                  <td className={styles['right-cell']}>40</td>
+                  <td className={styles['right-cell']}>40</td>
+                  <td className={styles['right-cell']}>40</td>
+                  <td className={styles['right-cell']}>40</td>
+                  <td className={styles['right-cell']}>40</td>
+                  <td className={styles['right-cell']}>40</td>
+                  <td className={styles['right-cell']}>40</td>
+                </tr>
+                <tr className={styles['body-row']}>
+                  <td className={styles['left-cell']}>Leilani Boyce</td>
+                  <td className={styles['left-cell']}>Principal I</td>
+                  <td className={styles['left-cell']}>UX Architect</td>
+                  <td className={styles['right-cell']}>40</td>
+                  <td className={styles['right-cell']}>40</td>
+                  <td className={styles['right-cell']}>40</td>
+                  <td className={styles['right-cell']}>40</td>
+                  <td className={styles['right-cell']}>40</td>
+                  <td className={styles['right-cell']}>40</td>
+                  <td className={styles['right-cell']}>40</td>
+                </tr>
+                <tr className={styles['body-row']}>
+                  <td className={styles['left-cell']}>Rachael Bisbo</td>
+                  <td className={styles['left-cell']}>Staff I</td>
+                  <td className={styles['left-cell']}>UX Architect</td>
+                  <td className={styles['right-cell']}>40</td>
+                  <td className={styles['right-cell']}>40</td>
+                  <td className={styles['right-cell']}>40</td>
+                  <td className={styles['right-cell']}>40</td>
+                  <td className={styles['right-cell']}>40</td>
+                  <td className={styles['right-cell']}>40</td>
+                  <td className={styles['right-cell']}>40</td>
+                </tr>
+                <tr className={styles['body-row']}>
+                  <td className={styles['left-cell']}>Rachel Cowles</td>
+                  <td className={styles['left-cell']}>Associate II</td>
+                  <td className={styles['left-cell']}>Designer</td>
+                  <td className={styles['right-cell']}>40</td>
+                  <td className={styles['right-cell']}>40</td>
+                  <td className={styles['right-cell']}>40</td>
+                  <td className={styles['right-cell']}>40</td>
+                  <td className={styles['right-cell']}>40</td>
+                  <td className={styles['right-cell']}>40</td>
+                  <td className={styles['right-cell']}>40</td>
+                </tr>
+                <tr className={styles['body-row']}>
+                  <td className={styles['left-cell']}>Sarah Doran</td>
+                  <td className={styles['left-cell']}>Staff II</td>
+                  <td className={styles['left-cell']}>Front-end Developer</td>
+                  <td className={styles['right-cell']}>40</td>
+                  <td className={styles['right-cell']}>40</td>
+                  <td className={styles['right-cell']}>40</td>
+                  <td className={styles['right-cell']}>40</td>
+                  <td className={styles['right-cell']}>40</td>
+                  <td className={styles['right-cell']}>40</td>
+                  <td className={styles['right-cell']}>40</td>
+                </tr>
+                <tr className={styles['body-row']}>
+                  <td className={styles['left-cell']}>Van Wilson</td>
+                  <td className={styles['left-cell']}>Senior II</td>
+                  <td className={styles['left-cell']}>Front-end Developer</td>
+                  <td className={styles['right-cell']}>40</td>
+                  <td className={styles['right-cell']}>40</td>
+                  <td className={styles['right-cell']}>40</td>
+                  <td className={styles['right-cell']}>40</td>
+                  <td className={styles['right-cell']}>40</td>
+                  <td className={styles['right-cell']}>40</td>
+                  <td className={styles['right-cell']}>40</td>
+                </tr>
+              </tbody>
+            </table>
+            <div className={styles['actions']}>
+              <a href="">+ Add New Teammate</a>
+            </div>
+            <div className={styles['actions']}>
+              <a href="">Edit History</a>
+            </div>
+          </div>
         )}
       </div>
     );
